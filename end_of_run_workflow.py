74ef4a371a78d671f4e9960ee3dd82a379d7d905
Jun Aishima <jaishima@bnl.gov>
2025-02-25 17:18:38 -0500

    remove not-useful and resource-hungry task
     * after issues seen around 2025-02-25 with heavy access of Tiled by the
       LiX end-of-run workflow

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,6 +1,5 @@
 import prefect
 from prefect import flow, task, get_run_logger
-from data_validation import general_data_validation
 
 @task
 def log_completion():
@@ -11,5 +10,4 @@ def log_completion():
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
 
-    general_data_validation(beamline_acronym="lix", uid=uid)
     log_completion()
